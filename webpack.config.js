8cd3cedc08d10537894d06c69a8fa236ba49286e
Miroslav <derevianko_miroslav@ukr.net>
2018-08-27 22:08:25 +0300

    Change context in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ const htmlWebpackPlugin = new HtmlWebPackPlugin({
 
 module.exports = {
   entry: './src/index.js',
+  context: __dirname,
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
