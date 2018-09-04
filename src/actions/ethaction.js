818a8fe93d2f0035f0d17daf08906f7d675bd5a4
MiroslavDerevianko <dereviankomiroslav77@gmail.com>
2018-09-04 19:15:42 +0300

    Connect balance & deposit actions

--- src/actions/ethaction.js
+++ src/actions/ethaction.js
@@ -1,6 +1,9 @@
+import { checkAndGetToken, logout, refreshToken } from "./authaction";
+import { apiurl } from "../appconfig";
+
 export const ETH_BALANCE_START = 'ETH_BALANCE_START';
 export const ETH_BALANCE_SUCCESS = 'ETH_BALANCE_SUCCESS';
-export const ETH_BALANCE_FAILED = 'ETH_BALANCE_SUCCESS';
+export const ETH_BALANCE_FAILED = 'ETH_BALANCE_FAILED';
 
 export const TAXI_BALANCE_START = 'TAXI_BALANCE_START';
 export const TAXI_BALANCE_SUCCESS = 'TAXI_BALANCE_SUCCESS';
@@ -63,22 +66,89 @@ let TaxiBalance = 56;
 export const getEthBalance = () => (dispatch, getState) => {
     dispatch(ethStart());
 
-    setTimeout(() => {
+    /* Test timeout */
+    /* setTimeout(() => {
         dispatch(ethSuccess(EthBalance));
-    }, 1000);
+    }, 1000); */
+
+    /* Cant test this (need eth purse) */
+    const token = checkAndGetToken(dispatch, getState);
+    if (token) {
+        fetch(`${apiurl}/api/balance/ethereum`, {
+            method: 'GET',
+            headers: new Headers({
+                'Authorization': `Bearer ${token.auth_token}`
+            })
+        })
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json()
+                } else if (res.status === 401) {
+                    dispatch(refreshToken(token, getEthBalance));
+                } else if (res.status === 400) {
+                    return res.json();
+                } else {
+                    throw new Error(res.statusText);
+                }
+            })
+            .then(data => {
+                if (Array.isArray(data[Object.keys(data)[0]])) {
+                    dispatch(ethFailed(data[Object.keys(data)[0]][0]));
+                } else {
+                    dispatch(ethSuccess(data));
+                }
+            })
+            .catch(error => dispatch(ethFailed(error.message)));
+    } else {
+        dispatch(logout());
+    }
 }
 
 export const getTaxiBalance = () => (dispatch, getState) => {
     dispatch(taxiStart());
-
-    setTimeout(() => {
+    /* Test timeout */
+    /* setTimeout(() => {
         dispatch(taxiSuccess(TaxiBalance));
-    }, 1000);
+    }, 1000); */
+
+    const token = checkAndGetToken(dispatch, getState);
+    if (token) {
+        fetch(`${apiurl}/api/balance/tokens`, {
+            method: 'GET',
+            headers: new Headers({
+                'Authorization': `Bearer ${token.auth_token}`
+            })
+        })
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json()
+                } else if (res.status === 401) {
+                    dispatch(refreshToken(token, getTaxiBalance));
+                } else if (res.status === 400) {
+                    return res.json();
+                } else {
+                    throw new Error(res.statusText);
+                }
+            })
+            .then(data => {
+                if (data) {
+                    if (Array.isArray(data[Object.keys(data)[0]])) {
+                        dispatch(taxiFailed(data[Object.keys(data)[0]][0]));
+                    } else {
+                        dispatch(taxiSuccess(data));
+                    }
+                }
+            })
+            .catch(error => dispatch(taxiFailed(error.message)));
+    } else {
+        dispatch(logout());
+    }
 }
 
 export const depositToTaxiBalance = (value) => (dispatch, getState) => {
     dispatch(changeStart());
-    const val = Number.parseFloat(value);
+    /* Test timeout */
+    /* const val = Number.parseFloat(value);
     setTimeout(() => {
         if ((EthBalance - val) >= 0) {
             EthBalance -= val;
@@ -89,5 +159,38 @@ export const depositToTaxiBalance = (value) => (dispatch, getState) => {
         } else {
             dispatch(changeFailed('Not enough balance'));
         }
-    }, 1000);
+    }, 1000); */
+
+    const token = checkAndGetToken(dispatch, getState);
+    if (token) {
+        fetch(`${apiurl}/api/deposit`, {
+            method: 'POST',
+            headers: new Headers({
+                'Authorization': `Bearer ${token.auth_token}`,
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify({ value })
+        })
+            .then(res => {
+                if (res.status === 200) {
+                    dispatch(changeSuccess('Deposit was succesfull'));
+                } else if (res.status === 400) {
+                    return res.json();
+                } else if (res.status === 401) {
+                    dispatch(refreshToken(token, depositToTaxiBalance, value));
+                } else {
+                    throw new Error(res.statusText);
+                }
+            })
+            .then(data => {
+                if (data) {
+                    if (Array.isArray(data[Object.keys(data)[0]])) {
+                        dispatch(changeFailed(data[Object.keys(data)[0]][0]));
+                    }
+                }
+            })
+            .catch(error => dispatch(changeFailed(error.message)));
+    } else {
+        dispatch(logout());
+    }
 }
